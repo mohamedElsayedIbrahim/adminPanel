b83539feb65dbb37406719d449ee86ee577e58f2
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2022-11-24 18:02:36 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,11 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'role:super-admin', 'role:admin'])->name('dashboard');
+
+Route::get('/admin', function () {
+    return view('admin.index');
+})->middleware(['auth', 'role:super-admin'])->name('admin.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
